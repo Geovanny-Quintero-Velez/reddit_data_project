72d0e508adf4c82aa42c2916c3f0ac277b0e8e15
Geovanny <geovannyalexander07@gmail.com>
2025-05-11 08:17:09 -0500

    add etl procesess for extract, transform and initial load

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,12 +1,17 @@
-from etls.reddit_etl import connect_reddit, extract_posts
-from utils.constants import CLIENT_ID, SECRET
+from etls.reddit_etl import connect_reddit, extract_posts, load_data_to_csv, transform_data
+from utils.constants import CLIENT_ID, OUTPUT_PATH, SECRET
+import pandas as pd
 
-
-def reddit_pipeline(filename: str, subreddit: str, time_filter='day', limit=None):
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
     #Connect reddit instance
     instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
     #Extract
     posts = extract_posts(instance, subreddit, time_filter, limit)
+    post_df = pd.DataFrame(posts)
     #Transform
-
+    post_df = transform_data(post_df)
     #Load to csv
+    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+    load_data_to_csv(post_df, file_path)
+
+    return file_path
